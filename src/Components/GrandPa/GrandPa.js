3966424ceb28af7a41b05c6a92932a59680e108f
sarafat27 <sarafat2769@gmail.com>
2022-04-03 19:26:24 +0600

    context API added

--- src/Components/GrandPa/GrandPa.js
+++ src/Components/GrandPa/GrandPa.js
@@ -1,8 +1,22 @@
-import React, { useState } from 'react';
+import React, { createContext, useState } from 'react';
 import Aunty from '../Aunty/Aunty';
 import Father from '../Father/Father';
 import Uncle from '../Uncle/Uncle';
 import './GrandPa.css'
+/* context api steps:
+1.call createContext with a default value
+2.set a variable of the context with uppercase
+3.Make sure you export the context to use it in other places
+4.wrap your child content using contextName.provider
+5.provide a value
+6.to use the context in child component:
+   a.use useContext hook and pass the context name as a value in the hook 
+7.we can send array,function,variable,object everything through context API   
+ */
+
+
+export const RingContext = createContext('Ring')
+
 const GrandPa = () => {
     const [house, setHouse] = useState(1);
     const ornaments = 'Diamond ring';
@@ -10,17 +24,19 @@ const GrandPa = () => {
         setHouse(house + 1);
     }
     return (
-        <div className='grandpa'>
-            <h3>Grandpa</h3>
-            <p>House: {house}</p>
-            <button onClick={buyHouse}>Buy a house</button>
-            <div style={{ display: 'flex' }}>
-                <Father house={house} ornaments={ornaments}></Father>
-                <Uncle house={house}></Uncle>
-                <Aunty house={house}></Aunty>
-            </div>
+        <RingContext.Provider value={[house, setHouse]}>
+            <div className='grandpa'>
+                <h3>Grandpa</h3>
+                <p>House: {house}</p>
+                <button onClick={buyHouse}>Buy a house</button>
+                <div style={{ display: 'flex' }}>
+                    <Father house={house} ornaments={ornaments}></Father>
+                    <Uncle house={house}></Uncle>
+                    <Aunty house={house}></Aunty>
+                </div>
 
-        </div>
+            </div>
+        </RingContext.Provider>
     );
 };
 
--- src/Components/Special/Special.js
+++ src/Components/Special/Special.js
@@ -1,10 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RingContext } from '../GrandPa/GrandPa';
 
 const Special = ({ ornaments }) => {
+    const [house, setHouse] = useContext(RingContext);
     return (
         <div>
             <h5>Special</h5>
-            <p>gift: {ornaments}</p>
+            <p>Gift: {ornaments}</p>
+            <p>House: {house}</p>
+            <button onClick={() => setHouse(house + 1)}>Buy a house</button>
         </div>
     );
 };
